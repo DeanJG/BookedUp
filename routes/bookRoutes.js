fc6a8dd489b2cae1856ea23f61a033ec772fd1bb
nina <ninaw1@uci.edu>
2019-05-22 21:29:10 -0700

    added routes in

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,38 @@
 const db = require('../models')
 
 module.exports = app => {
-    // bookClub routes go here...
+    // bookRoutes routes go here...
+    // GET all books
+    app.get('/books', (req, res) => {
+        Book.findAll()
+            .then(books => res.json(books))
+            .catch(e => console.log(e))
+    })
+    // GET a book 
+    app.get('/books/:id', (req, res) => {
+        Book.findOne({ where: {id: req.params.id } })
+            .then(book => res.json(book))
+            .catch(e => console.log(e))
+    })
+
+    // POST a book 
+    app.post('/books', (req, res) => {
+        Book.create(req.body)
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
+
+    // PUT a book 
+    app.put('/books/:id', (req, res) => {
+        Book.update(req.body, { where: { id: req.params.id } })
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
+
+    // DELETE a book 
+    app.delete('/books/:id', (req, res) => {
+        Book.destroy({ where: { id: req.params.id } })
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,38 @@
 const db = require('../models')
 
 module.exports = app => {
-    // bookClub routes go here...
+    // user routes go here...
+    // GET all users
+    app.get('/users', (req, res) => {
+        Book.findAll()
+            .then(users => res.json(users))
+            .catch(e => console.log(e))
+    })
+    // GET a user
+    app.get('/users/:id', (req, res) => {
+        Book.findOne({ where: {id: req.params.id } })
+            .then(user => res.json(user))
+            .catch(e => console.log(e))
+    })
+
+    // POST a user
+    app.post('/users', (req, res) => {
+        Book.create(req.body)
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
+
+    // PUT a user
+    app.put('/users/:id', (req, res) => {
+        Book.update(req.body, { where: { id: req.params.id } })
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
+
+    // DELETE a user 
+    app.delete('/users/:id', (req, res) => {
+        Book.destroy({ where: { id: req.params.id } })
+            .then(_ => res.sendStatus(200))
+            .catch(e => console.log(e))
+    })
 }

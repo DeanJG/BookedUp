835987864be8fa4dc6d62be88720b4b4bc1ac1ad
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-06-02 11:38:27 -0700

    quick bookclub/user seed update for column change

--- db/bookClubSeed.sql
+++ db/bookClubSeed.sql
@@ -6,6 +6,6 @@ CREATE DATABASE bookedup_db;
 
 USE bookedup_db;
 
-INSERT INTO bookclubs (`name`, book, users, createdAt, updatedAt, userId)
+INSERT INTO bookclubs (`name`, bookId, userIds, createdAt, updatedAt, userId)
 VALUES ("We're Wizards, Harry!", '1', '1,2,3,4,8', '2019-06-02 11:12:10', '2019-06-02 11:12:10', 8), 
 ("Maybe This Will End Better", '5', '1,2,3,4,7,9', '2019-06-02 11:12:10', '2019-06-02 11:12:10', 1);

4b512606fd1860dc4ea50bb0687a4fcc1eb4e011
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-06-04 18:04:35 -0700

    added basic user/friends fetch and fixed route sources

--- routes/bookClubRoutes.js
+++ routes/bookClubRoutes.js
@@ -4,34 +4,34 @@ module.exports = app => {
     // bookRoutes routes go here...
     // GET all bookclubs
     app.get('/bookclubs', (req, res) => {
-        db.Book.findAll()
+        db.BookClub.findAll()
             .then(bookclubs => res.json(bookclubs))
             .catch(e => console.log(e))
     })
     // GET a bookclubs 
     app.get('/bookclubs/:id', (req, res) => {
-        db.Book.findOne({ where: {id: req.params.id } })
+        db.BookClub.findOne({ where: {id: req.params.id } })
             .then(bookclub => res.json(bookclub))
             .catch(e => console.log(e))
     })
 
     // POST a bookclubs 
     app.post('/bookclubs', (req, res) => {
-        db.Book.create(req.body)
+        db.BookClub.create(req.body)
             .then(_ => res.sendStatus(200))
             .catch(e => console.log(e))
     })
 
     // PUT a bookclubs 
     app.put('/bookclubs/:id', (req, res) => {
-        db.Book.update(req.body, { where: { id: req.params.id } })
+        db.BookClub.update(req.body, { where: { id: req.params.id } })
             .then(_ => res.sendStatus(200))
             .catch(e => console.log(e))
     })
 
     // DELETE a bookclubs 
     app.delete('/bookclubs/:id', (req, res) => {
-        db.Book.destroy({ where: { id: req.params.id } })
+        db.BookClub.destroy({ where: { id: req.params.id } })
             .then(_ => res.sendStatus(200))
             .catch(e => console.log(e))
     })
